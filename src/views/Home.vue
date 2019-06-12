0ce7c92b04e88ada24f872fc6f06edec12523172
danizaghian <dani.zaghian@gmail.com>
2019-06-12 14:04:15 -0700

    adds recipes edit and update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,16 +12,28 @@
 		<button v-on:click="createRecipe()">Create</button>
 
     <h1>{{ message }}</h1>
+
     <div v-for="recipe in recipes">
     	<h3>Title: {{ recipe.title }}</h3>
     	<img v-bind:src="recipe.image_url" alt="">
     	<div>
-    		<button v-on:click="currentRecipe = recipe">More Info</button>
+    		<button v-on:click="showRecipe(recipe)">More Info</button>
     	</div>
     	<div v-if="recipe === currentRecipe">
 	    	<p>Ingredients: {{ currentRecipe.ingredients }}</p>
 	    	<p>Directions: {{ currentRecipe.directions }}</p>
 	    	<p>Prep Time: {{ currentRecipe.prep_time }}</p>
+	    	<h4>Edit Recipe</h4>
+	    	<div>
+	    		Title: <input type="text" v-model="recipe.title"><br>
+
+	    		Recipe.title value: {{recipe.title}} <br>
+	    		Prep Time: <input type="number" v-model="recipe.prep_time"><br>
+	    		Ingredients: <input type="text" v-model="recipe.ingredients"><br>
+	    		Directions: <input type="text" v-model="recipe.directions"><br>
+	    		Image Url: <input type="text" v-model="recipe.image_url"><br>
+	    		<button v-on:click="updateRecipe(recipe)">Update</button>
+	    	</div>
     	</div>
     </div>
 
@@ -61,7 +73,7 @@ export default {
   			directions: this.newRecipeDirections,
   			image_url: this.newRecipeImageUrl,
   			prep_time: this.newRecipePrepTime
-  		}
+  		};
   		axios.post("/api/recipes", params).then(response => {
   			console.log("Success!", response.data);
   			this.recipes.push(response.data);
@@ -70,6 +82,27 @@ export default {
   			this.newRecipeIngredients = "";
   			this.newRecipeDirections = "";
   			this.newRecipeImageUrl = "";
+  		});
+  	},
+  	showRecipe: function(recipe) {
+  		if (this.currentRecipe === recipe) {
+  			this.currentRecipe = null;
+  		} else {
+				this.currentRecipe = recipe;
+  		}
+  	},
+  	updateRecipe: function(recipe) {
+  		// send a patch request to the backend to update this recipe!
+  		var params = {
+  			title: recipe.title,
+  			ingredients: recipe.ingredients,
+  			directions: recipe.directions,
+  			image_url: recipe.image_url,
+  			prep_time: recipe.prep_time
+  		};
+  		axios.patch("/api/recipes/" + recipe.id, params).then(response => {
+  			console.log("Success!", response.data);
+  			recipe = response.data;
   		});
   	}
   }
