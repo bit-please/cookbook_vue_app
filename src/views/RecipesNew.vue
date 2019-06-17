231ab481afb786367ef3405db6bc4e0a7d922339
danizaghian <dani.zaghian@gmail.com>
2019-06-17 12:07:46 -0700

    adds form syntax to new recipe form

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -7,14 +7,31 @@
       <li v-for="error in errors">{{ error }}</li>
     </ul>
 
-		<div>
-			Title: <input type="text" v-model="newRecipeTitle"><br>
-			Prep Time: <input type="number" v-model="newRecipePrepTime"><br>
-			Ingredients: <input type="text" v-model="newRecipeIngredients"><br>
-			Directions: <input type="text" v-model="newRecipeDirections"><br>
-			Image Url: <input type="text" v-model="newRecipeImageUrl">
-		</div>
-		<button class="btn btn-success" v-on:click="createRecipe()">Create</button>
+    <form v-on:submit.prevent="submit()">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="title">Title</label>
+          <input type="text" class="form-control" id="title" placeholder="Cookies" v-model="newRecipeTitle">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="prepTime">Prep Time</label>
+          <input type="number" class="form-control" id="prepTime" placeholder="20" v-model="newRecipePrepTime">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="ingredients">Ingredients</label>
+        <input type="text" class="form-control" id="ingredients" placeholder="Sugar, water, butter, eggs..." v-model="newRecipeIngredients">
+      </div>
+      <div class="form-group">
+        <label for="directions">Directions</label>
+        <input type="text" class="form-control" id="directions" placeholder="Mix in bowl and bake at 350 for 10 mins..." v-model="newRecipeDirections">
+      </div>
+      <div class="form-group">
+        <label for="imageUrl">Image</label>
+        <input type="text" class="form-control" id="imageUrl" placeholder="Paste image link" v-model="newRecipeImageUrl">
+      </div>
+      <button type="submit" class="btn btn-success">Create</button>
+    </form>
     
   </div>
 </template>
@@ -36,7 +53,7 @@ export default {
   created: function() {
   },
   methods: {
-  	createRecipe: function() {
+  	submit: function() {
   		// make a post request via axios to create a new recipe in our database!
   		var params = {
   			title: this.newRecipeTitle,
